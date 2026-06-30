fece88cc944f5c0f07e3524fe16ffe568e6cf680
Amaan Pasha <amaanpasha23@gmail.com>
2026-07-01 01:48:06 +0530

    adjusted the backend over here after the supabase authentication is done over here

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+import  Express from "express";
+import cors from "cors";
+const app = Express();
+
+app.use(Express.json());
+app.use(cors());
+
+app.post("/buy", middleware, (req, res)=>{
+     
+});
+
+app.post("/sell",middleware, (req, res)=>{
+    
+});
+
+app.post("/split",middleware, (req, res)=>{
+    
+});
+
+app.post("/merge",middleware, (req, res)=>{
+    
+});
+
+app.get("/balance",middleware, (req, res)=>{
+    
+})
+app.get("/position", middleware, (req, res)=>{
+    
+})
+app.get("/history", middleware, (req, res)=>{
+    
+})
+
+
+app.listen(5000,()=> console.log("app is listening"));
---
+++ apps/backend/middleware.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from "express";
+
+export function middleware(req : Request, res : Response){
+    
+}
